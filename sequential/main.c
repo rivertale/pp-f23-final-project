1ef68212970225cc82dc12f8dbb5edf2370c681a
LiMinChu914 <andy27564@gmail.com>
2023-12-17 01:29:25 +0800

    feat: use threshold as terminate condition(modify Kmean(), classify_points())

--- sequential/main.c
+++ sequential/main.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <malloc.h>
+#include "common.h"
+#include "profile.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 #include <stdio.h>
-#include "common.h"
-#include "profile.h"
+#include <malloc.h>
 
 typedef struct Color4
 {
@@ -24,16 +23,16 @@ typedef struct Image
     FILE *handle;
 } Image;
 
-static int 
+static int
 load_image_info(Image *image, char *path)
 {
     int result = 0;
     clear_memory(image, sizeof(*image));
     FILE *file_handle = fopen(path, "rb");
-    if(file_handle)
+    if (file_handle)
     {
         int width, height, channel_count;
-        if(stbi_info_from_file(file_handle, &width, &height, &channel_count))
+        if (stbi_info_from_file(file_handle, &width, &height, &channel_count))
         {
             result = 1;
             image->width = width;
@@ -51,26 +50,26 @@ load_image_info(Image *image, char *path)
 static void
 free_image_info(Image *image)
 {
-    if(image)
+    if (image)
     {
         fclose(image->handle);
         clear_memory(image, sizeof(*image));
     }
 }
 
-static void 
+static void
 load_image_data(Color4 *output, Image *image)
 {
     int image_width, image_height, channel_count;
     unsigned char *input_pixels = stbi_load_from_file(image->handle, &image_width, &image_height, &channel_count, 3);
-    if(input_pixels)
+    if (input_pixels)
     {
         Color4 *output_pixel = output;
-        for(int y = 0; y < image->height; ++y)
+        for (int y = 0; y < image->height; ++y)
         {
-            for(int x = 0; x < image->width; ++x)
+            for (int x = 0; x < image->width; ++x)
             {
-                unsigned char *channels = input_pixels + (image_height - 1 - y)*image_width*3 + x*3;
+                unsigned char *channels = input_pixels + (image_height - 1 - y) * image_width * 3 + x * 3;
                 output_pixel->r = channels[0];
                 output_pixel->g = channels[1];
                 output_pixel->b = channels[2];
@@ -85,16 +84,16 @@ load_image_data(Color4 *output, Image *image)
     }
 }
 
-static int 
+static int
 write_image(char *path, Color4 *bitmap, int width, int height)
 {
     int result = 0;
     int *data = (int *)malloc(height * width * sizeof(int));
-    if(data)
+    if (data)
     {
-        for(int y = 0; y < height; ++y)
+        for (int y = 0; y < height; ++y)
         {
-            for(int x = 0; x < width; ++x)
+            for (int x = 0; x < width; ++x)
             {
                 Color4 *channel = bitmap + y * width + x;
                 int r = channel->r;
@@ -110,105 +109,110 @@ write_image(char *path, Color4 *bitmap, int width, int height)
     return result;
 }
 
-
-void AllocateRandomClusters(Color4 * centroid, Color4 * pixels, short clustercount)
+void AllocateRandomClusters(Color4 *centroid, Color4 *pixels, short clustercount)
 {
-  printf("Initial randomized cluster centres: \n\n");
-  // Initializing clusters
-  //srand(time(NULL)); 
-  for(int i = 0; i < clustercount; i++)
-  {
-    centroid[i].r = pixels[i].r;
-    centroid[i].g = pixels[i].g;
-    centroid[i].b = pixels[i].b;
-    centroid[i].a = pixels[i].a;
-    /*centroid[i].r = rand()%256;
-    centroid[i].g = rand()%256;
-    centroid[i].b = rand()%256;
-    centroid[i].a = rand()%256;*/
-    /*printf("%d, ", centroid[i].r);
-    printf("%d, ", centroid[i].g);
-    printf("%d, ", centroid[i].b);
-    printf("%d, ", centroid[i].a);
-    printf("\n");*/
-  }
+    printf("Initial randomized cluster centres: \n\n");
+    // Initializing clusters
+    // srand(time(NULL));
+    for (int i = 0; i < clustercount; i++)
+    {
+        centroid[i].r = pixels[i].r;
+        centroid[i].g = pixels[i].g;
+        centroid[i].b = pixels[i].b;
+        centroid[i].a = pixels[i].a;
+        /*centroid[i].r = rand() % 256;
+        centroid[i].g = rand() % 256;
+        centroid[i].b = rand() % 256;
+        centroid[i].a = rand() % 256;
+        printf("%d, ", centroid[i].r);
+        printf("%d, ", centroid[i].g);
+        printf("%d, ", centroid[i].b);
+        printf("%d, ", centroid[i].a);
+        printf("\n");*/
+    }
 }
 
-void classify_points(Color4 * centroid, int * label, Color4 * pixels, int  cluster_count, int total_pixel){
+void classify_points(Color4 *centroid, int *pre_label, int *label, Color4 *pixels, int *migration_count, int cluster_count, int total_pixel)
+{
 
-    for(int i = 0; i < total_pixel; i++)
-    {   
+    for (int i = 0; i < total_pixel; i++)
+    {
         int index = -1;
         int min_dist = 1000000;
-        for(int j = 0; j < cluster_count; j++){
+        for (int j = 0; j < cluster_count; j++)
+        {
             int dist = 0;
             dist = dist + pow(pixels[i].r - centroid[j].r, 2);
             dist = dist + pow(pixels[i].g - centroid[j].g, 2);
             dist = dist + pow(pixels[i].b - centroid[j].b, 2);
             dist = dist + pow(pixels[i].a - centroid[j].a, 2);
             dist = sqrt(dist);
-            //printf("%d ",dist);
-            if(dist < min_dist){
+            // printf("%d ",dist);
+            if (dist < min_dist)
+            {
                 index = j;
                 min_dist = dist;
-            }   
+            }
         }
-        //printf("%d\n",index);
+        // printf("%d\n",index);
+        if (index != pre_label[i])
+            (*migration_count)++;
         label[i] = index;
     }
 }
 
-void update_centroid(Color4 * centroid, int * label, Color4 * pixels, int  cluster_count, int total_pixel){
-    
-    Color4_SUM * label_sum = (Color4_SUM*) calloc(cluster_count , sizeof(Color4_SUM));
-    int* label_count = (int*) calloc(cluster_count , sizeof(int));
+void update_centroid(Color4 *centroid, int *label, Color4 *pixels, int cluster_count, int total_pixel)
+{
 
-    //Calculate each number of each label
-    for(int i = 0; i < total_pixel; i++)
-    {   
-        label_count[label[i]] +=1;
+    Color4_SUM *label_sum = (Color4_SUM *)calloc(cluster_count, sizeof(Color4_SUM));
+    int *label_count = (int *)calloc(cluster_count, sizeof(int));
+
+    // Calculate each number of each label
+    for (int i = 0; i < total_pixel; i++)
+    {
+        label_count[label[i]] += 1;
     }
 
-    for(int i = 0; i < total_pixel; i++)
-    {   
+    for (int i = 0; i < total_pixel; i++)
+    {
         label_sum[label[i]].r += pixels[i].r;
         label_sum[label[i]].g += pixels[i].g;
         label_sum[label[i]].b += pixels[i].b;
         label_sum[label[i]].a += pixels[i].a;
     }
 
-    for(int i = 0; i < cluster_count; i++)
-    { 
-        printf("%d ",label_count[i]);
-        printf("%ld ",label_sum[i].r);
-        printf("%ld ",label_sum[i].g);
-        printf("%ld ",label_sum[i].b);
-        printf("%ld \n",label_sum[i].a);
-    } 
+    for (int i = 0; i < cluster_count; i++)
+    {
+        printf("%d ", label_count[i]);
+        printf("%ld ", label_sum[i].r);
+        printf("%ld ", label_sum[i].g);
+        printf("%ld ", label_sum[i].b);
+        printf("%ld \n", label_sum[i].a);
+    }
     printf("\n");
-    
 
-    for(int i = 0; i < cluster_count; i++)
-    {   
-        //printf("%ld",label_sum[i].r/label_count[i]);
-        if(label_sum[i].r != 0 && label_count[i] != 0)
-            centroid[i].r = label_sum[i].r/label_count[i];
-        if(label_sum[i].g != 0 && label_count[i] != 0)
-            centroid[i].g = label_sum[i].g/label_count[i];
-        if(label_sum[i].b != 0 && label_count[i] != 0)
-            centroid[i].b = label_sum[i].b/label_count[i];
-        if(label_sum[i].a != 0 && label_count[i] != 0)
-        centroid[i].a = label_sum[i].a/label_count[i];
+    for (int i = 0; i < cluster_count; i++)
+    {
+        // printf("%ld",label_sum[i].r/label_count[i]);
+        if (label_sum[i].r != 0 && label_count[i] != 0)
+            centroid[i].r = label_sum[i].r / label_count[i];
+        if (label_sum[i].g != 0 && label_count[i] != 0)
+            centroid[i].g = label_sum[i].g / label_count[i];
+        if (label_sum[i].b != 0 && label_count[i] != 0)
+            centroid[i].b = label_sum[i].b / label_count[i];
+        if (label_sum[i].a != 0 && label_count[i] != 0)
+            centroid[i].a = label_sum[i].a / label_count[i];
     }
 
     free(label_sum);
     free(label_count);
 }
 
-void output_result(int * label, Color4 * output, Color4 * centroid, int cluster_count, int total_pixel){
+void output_result(int *label, Color4 *output, Color4 *centroid, int cluster_count, int total_pixel)
+{
 
-    for(int i = 0; i < total_pixel; i++)
-    {   
+    for (int i = 0; i < total_pixel; i++)
+    {
         output[i].r = centroid[label[i]].r;
         output[i].g = centroid[label[i]].g;
         output[i].b = centroid[label[i]].b;
@@ -216,30 +220,41 @@ void output_result(int * label, Color4 * output, Color4 * centroid, int cluster_
     }
 }
 
-static void 
-Kmean(Color4 *output, Color4 *pixels, int width, int height, 
-                         int cluster_count, int max_iteration, float migration_threshold,
-                         int *out_iteration)
-{   
-    printf("%d\n",cluster_count);
+static void
+Kmean(Color4 *output, Color4 *pixels, int width, int height,
+      int cluster_count, int max_iteration, float migration_threshold,
+      int *out_iteration)
+{
     int pixel_count = width * height;
-    Color4 *centroid = (Color4 *) malloc(cluster_count * sizeof(Color4));
-    int* label = (int*) malloc(pixel_count * sizeof(int));
+    Color4 *centroid = (Color4 *)malloc(cluster_count * sizeof(Color4));
+    int *label = (int *)malloc(pixel_count * sizeof(int));
+    int *previous_label = (int *)calloc(pixel_count, sizeof(int));
 
     AllocateRandomClusters(centroid, pixels, cluster_count);
 
-    for(int i = 0; i < max_iteration; i++)
-    {   
-        printf("%d\n",i);
+    int migration_count;
+    int i = 0;
+    while (i++ < max_iteration)
+    {
+        migration_count = 0;
+
         printf("classify_points\n");
-        classify_points(centroid, label, pixels, cluster_count, pixel_count);
+        classify_points(centroid, previous_label, label, pixels, &migration_count, cluster_count, pixel_count);
         printf("update_centroid\n");
         update_centroid(centroid, label, pixels, cluster_count, pixel_count);
-        
+
+        if (migration_count / (float)pixel_count < migration_threshold)
+        {
+            *out_iteration = i;
+            break;
+        }
+        memcpy(previous_label, label, pixel_count * sizeof(int));
     }
+    printf("out_iteration: %d\n", *out_iteration);
     output_result(label, output, centroid, cluster_count, pixel_count);
-    *out_iteration = max_iteration;
+
     free(label);
+    free(previous_label);
     free(centroid);
 }
 
@@ -252,28 +267,29 @@ int main(int arg_count, char **args)
     int max_iteration = 200;
     float migration_threshold = 0.01f;
 
-    for(; parsing_arg_index < arg_count; ++parsing_arg_index)
+    for (; parsing_arg_index < arg_count; ++parsing_arg_index)
     {
         char *option = args[parsing_arg_index];
-        if(option[0] != '-') break;
-        
-        if(option[1] == 'n' && option[2] == '=')
+        if (option[0] != '-')
+            break;
+
+        if (option[1] == 'n' && option[2] == '=')
         {
             cluster_count = atoi(option + 3);
         }
-        else if(option[1] == 'm' && option[2] == '=')
+        else if (option[1] == 'm' && option[2] == '=')
         {
             max_iteration = atoi(option + 3);
         }
-        else if(option[1] == 'r' && option[2] == '=')
+        else if (option[1] == 'r' && option[2] == '=')
         {
             migration_threshold = atof(option + 3);
         }
-        else if(option[1] == 'q' && option[2] == 0)
+        else if (option[1] == 'q' && option[2] == 0)
         {
             verbose = 0;
         }
-        else if(option[1] == 'h' && option[2] == 0)
+        else if (option[1] == 'h' && option[2] == 0)
         {
             show_usage = 1;
         }
@@ -282,8 +298,8 @@ int main(int arg_count, char **args)
             printf("unknown option '%s'\n", option);
         }
     }
-    
-    if(show_usage || (parsing_arg_index + 2 != arg_count))
+
+    if (show_usage || (parsing_arg_index + 2 != arg_count))
     {
         char *usage = "usage: kmean [option] ... input_path output_path\n"
                       "options:\n"
@@ -292,69 +308,76 @@ int main(int arg_count, char **args)
                       "    -r={threshold}      exit when the data point migration ratio between clusters exceeds this value (default is 0.01)\n"
                       "    -q                  quiet mode (no output)\n"
                       "    -h                  print this help information\n";
-        //NOTE: pass the string via '%s' to shut up the compiler warning
-        if(verbose) printf("%s", usage);
+        // NOTE: pass the string via '%s' to shut up the compiler warning
+        if (verbose)
+            printf("%s", usage);
         return 0;
     }
-    
+
     char *input_path = args[parsing_arg_index + 0];
     char *output_path = args[parsing_arg_index + 1];
     size_t output_path_len = string_len(output_path);
-    printf("Input_path: %s\nOutput_path:%s\n",input_path,output_path);
-    
-    if(output_path_len >= 4 && 
-       (output_path[output_path_len-4] == '.') && 
-       (output_path[output_path_len-3] == 'p' || output_path[output_path_len-3] == 'P') && 
-       (output_path[output_path_len-2] == 'n' || output_path[output_path_len-2] == 'N') && 
-       (output_path[output_path_len-1] == 'g' || output_path[output_path_len-1] == 'G'))
+    printf("Input_path: %s\nOutput_path:%s\n", input_path, output_path);
+
+    if (output_path_len >= 4 &&
+        (output_path[output_path_len - 4] == '.') &&
+        (output_path[output_path_len - 3] == 'p' || output_path[output_path_len - 3] == 'P') &&
+        (output_path[output_path_len - 2] == 'n' || output_path[output_path_len - 2] == 'N') &&
+        (output_path[output_path_len - 1] == 'g' || output_path[output_path_len - 1] == 'G'))
     {
         Image image;
-        if(load_image_info(&image, input_path))
+        if (load_image_info(&image, input_path))
         {
             Color4 *input = (Color4 *)malloc(image.width * image.height * sizeof(Color4));
             Color4 *output = (Color4 *)malloc(image.width * image.height * sizeof(Color4));
-            if(input && output)
+            if (input && output)
             {
                 load_image_data(input, &image);
                 int used_iteration;
                 unsigned long long start_time = get_microsecond_from_epoch();
-                Kmean(output, input, image.width, image.height, 
-                                         cluster_count, max_iteration, migration_threshold,  
-                                         &used_iteration);
+                Kmean(output, input, image.width, image.height,
+                      cluster_count, max_iteration, migration_threshold,
+                      &used_iteration);
                 unsigned long long end_time = get_microsecond_from_epoch();
-                if(verbose)
+                if (verbose)
                 {
                     printf("[summary]\n");
                     printf("    used iteration = %d\n", used_iteration);
                     printf("    time = %fs\n", (end_time - start_time) / 1000000.0f);
                 }
-                
-                if(write_image(output_path, output, image.width, image.height))
+
+                if (write_image(output_path, output, image.width, image.height))
                 {
                     // NOTE: success
                 }
                 else
                 {
-                    if(verbose) printf("ERROR: write '%s' failed\n", output_path);
+                    if (verbose)
+                        printf("ERROR: write '%s' failed\n", output_path);
                 }
             }
             else
             {
-                if(verbose) printf("ERROR: out of memory\n");
+                if (verbose)
+                    printf("ERROR: out of memory\n");
             }
-            
-            if(input) free(input);
-            if(output) free(output);
+
+            if (input)
+                free(input);
+            if (output)
+                free(output);
             free_image_info(&image);
         }
         else
         {
-            if(verbose) printf("ERROR: read '%s' failed\n", input_path);
+            if (verbose)
+                printf("ERROR: read '%s' failed\n", input_path);
         }
     }
     else
     {
-       if(verbose) printf("ERROR: output should end with '.png' extension\n");
+        if (verbose)
+            printf("ERROR: output should end with '.png' extension\n");
     }
     return 0;
 }
